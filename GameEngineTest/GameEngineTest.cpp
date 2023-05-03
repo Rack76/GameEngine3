1c2a2d159de207c1a2d405166eca53505b77ef4a
Karim Tit <rgterhy@hotmail.fr>
2023-05-03 15:27:54 +0200

    added test project

---
+++ GameEngineTest/GameEngineTest.cpp
@@ -0,0 +1,16 @@
+#include "CppUnitTest.h"
+#include "../GameEngine/src/TaskManager.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameEngineTest
+{
+	TEST_CLASS(GameEngineTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
